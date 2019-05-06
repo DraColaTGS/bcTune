c4a3a343154513c58548d7318b66bb86bf42b32d
DraColaTGS <50123084+DraColaTGS@users.noreply.github.com>
2019-05-06 12:12:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
   
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setGame(`   ||   `,'https://www.twitch.tv/zya2d_')
+    client.user.setGame(`-help    `,'https://www.twitch.tv/zya2d_')
     client.user.setStatus('ldle');
 });
 
